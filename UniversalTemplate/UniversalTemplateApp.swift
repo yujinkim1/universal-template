848fccad0f77708527e2ba2e0efc780f154d9a2c
yujinkim1 <yujin7621@me.com>
2025-04-24 18:48:24 +0900

    Initial commit
    SwiftUI 기반 멀티플랫폼 프로젝트 생성

---
+++ UniversalTemplate/UniversalTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalTemplateApp.swift
+//  UniversalTemplate
+//
+//  Created by Yujin Kim on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct UniversalTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
